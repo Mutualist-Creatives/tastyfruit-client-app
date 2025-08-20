2bb240ea8f0bc063dc93f3117c8d9b353594c386
Naufal Syarif <naufal.syarif0809@gmail.com>
2025-08-20 16:48:38 +0700

    chore: changing the wrapper

--- components/layout/conditional-wrapper.tsx
+++ components/layout/conditional-wrapper.tsx
@@ -1,7 +1,3 @@
-"use client";
-
-import { usePathname } from "next/navigation";
-
 interface ConditionalWrapperProps {
   children: React.ReactNode;
 }
@@ -9,20 +5,5 @@ interface ConditionalWrapperProps {
 export default function ConditionalWrapper({
   children,
 }: ConditionalWrapperProps) {
-  const pathname = usePathname();
-  const isFullWidthPage = pathname === "/";
-
-  if (isFullWidthPage) {
-    return (
-      <main className="flex-1 w-full min-h-screen bg-white">{children}</main>
-    );
-  }
-
-  return (
-    <main className="flex-1 w-full min-h-screen bg-white">
-      <div className="w-full h-auto mx-auto max-w-[1280px] px-4 sm:px-6 lg:px-20 font-nunito py-12">
-        {children}
-      </div>
-    </main>
-  );
+  return <main className="flex-1">{children}</main>;
 }
