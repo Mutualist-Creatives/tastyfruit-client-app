b6a0597a0cda4a45627c8f544e58a5c99e486d85
Naufal Syarif <naufal.syarif0809@gmail.com>
2025-08-20 16:46:07 +0700

    chore: changing the container

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,18 @@
 import HeroSection from "@/components/landing-page/hero-section";
 import KomitmenTasty from "@/components/landing-page/komitmen-tasty";
 import TastyUniverse from "@/components/landing-page/tasty-universe";
+import WhatToCook from "@/components/landing-page/what-to-cook";
+import Container from "@/components/layout/container";
 
 export default function LandingPage() {
   return (
-    <main className="flex-1 w-full min-h-screen bg-white">
-      <div className="w-full h-auto mx-auto max-w-[1280px] px-4 sm:px-6 lg:px-20 font-nunito py-12">
+    <>
+      <Container>
         <HeroSection />
         <KomitmenTasty />
-      </div>
+      </Container>
       <TastyUniverse />
-    </main>
+      <WhatToCook />
+    </>
   );
 }
