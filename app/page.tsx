1a3e1c7081e6c7e3b3f6b0923dca94068927e867
Naufal Syarif <naufal.syarif0809@gmail.com>
2025-08-21 13:32:35 +0700

    feat(landing page): adding new comps medsos tasty fruit

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 // app/page.tsx
 
+import Container from "@/components/layout/container";
+
 import HeroSection from "@/components/landing-page/hero-section";
 import KomitmenTasty from "@/components/landing-page/komitmen-tasty";
 import TastyUniverse from "@/components/landing-page/tasty-universe";
 import WhatToCook from "@/components/landing-page/what-to-cook";
-import Container from "@/components/layout/container";
+import MedsosTastyFruit from "@/components/landing-page/medsos-tasty-fruit";
 
 export default function LandingPage() {
   return (
@@ -15,6 +17,7 @@ export default function LandingPage() {
       </Container>
       <TastyUniverse />
       <WhatToCook />
+      <MedsosTastyFruit />
     </>
   );
 }
